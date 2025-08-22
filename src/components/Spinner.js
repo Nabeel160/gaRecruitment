67fa8b509b0a9dda98bfeb4e25ad5e052a1a9e2f
Nabeel160 <nabeelkhawar160@gmail.com>
2025-08-22 16:44:46 +0500

    Fixed loader

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,7 +1,7 @@
 export const Spinner = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-white">
-      <div className="w-16 h-16 border-4 border-red-900 border-t-transparent rounded-full animate-spin"></div>
+      <div className="w-16 h-16 border-4 border-[#b57571] border-t-transparent rounded-full animate-spin"></div>
     </div>
   );
 };
