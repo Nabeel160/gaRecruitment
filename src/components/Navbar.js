80c912f2a683faa3ea3eacdea88fcbd07d3a36e1
Nabeel160 <nabeelkhawar160@gmail.com>
2025-08-26 06:58:32 +0500

    Made logo clickable

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,9 +11,11 @@ export const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center relative">
           {/* Logo */}
+          <Link to="/">
           <div className="flex-shrink-0 text-white font-bold text-xl font-playpen">
             GA Recruitment
           </div>
+          </Link>
 
           {/* Desktop Links */}
           <div className="hidden md:flex space-x-6 items-center relative">
